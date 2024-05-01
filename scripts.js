0942964a537f6a028b17ab660dac851cbc36089e
Luke <luke.g.schwade@gmail.com>
2024-05-01 18:49:13 -0400

    visual framework finished

---
+++ scripts.js
@@ -0,0 +1,18 @@
+//UPDATE TIME
+class TimeWidget {
+    //Title is the title from which to pull from json file
+    constructor(title) {
+        //store title el
+
+        //store title
+        //store moreoptions menu el 
+        //store current time el
+        //store previous time el
+        //store value
+        //store 
+    }
+}
+
+//When user clicks on monthly, daily etc it must go through all the 
+//widget objects and update the values
+//If there was a server it should only load once
