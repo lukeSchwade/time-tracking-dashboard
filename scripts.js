3b297a4747c34817c5a27eecef5a388b327decc1
Luke <luke.g.schwade@gmail.com>
2024-05-02 16:21:28 -0400

    widgetobject logic completed

--- scripts.js
+++ scripts.js
@@ -1,36 +1,97 @@
 //UPDATE TIME
 class TimeWidget {
     //Title is the title from which to pull from json file
-    constructor(title) {
+    constructor(el) {
         //store title el
-
-        //store title
+        this.titleEl = el.querySelector('.title');
+        //store title for grabbing data from timeframe object
+        this.title = this.titleEl.textContent;
         //store moreoptions menu el 
+        this.moreOptions = el.querySelector('.more-options');
         //store current time el
+        this.currentTime = el.querySelector('.current-time');
         //store previous time el
+        this.previousTime = el.querySelector('.previous-time');
+        //store hourly/daily/monthly word
+        this.previousWord = el.querySelector('.previous-word')
         //store value
-        //store 
+        this.previousValue = el.querySelector('.value'); 
+        //Add event listener for more Options button
+    }
+    moreOptions () {
+
+    }
+    appendHours(inputNum){
+        //check if it should be hrs or hr and return the appended value
+        if (inputNum != 1) {
+            return inputNum + 'hrs';
+        } else {
+            return inputNum + 'hr';
+        }
+    }
+    //change word for timeframe
+    checkPastTense(buttonInput){
+        switch (buttonInput) {
+            case 'daily':
+                this.previousWord.textContent = 'Yesterday';
+                break;
+            case 'weekly':
+                this.previousWord.textContent = 'Last week';
+                break;
+            case 'monthly':
+                this.previousWord.textContent = 'Last month';
+            default:
+                break;
+        }
+    }
+    //Pulls numbers from the user data based on which timeframe was selected
+    updateData(timeFrame){
+        this.checkPastTense(timeFrame);
+        
     }
 }
 
-//When user clicks on monthly, daily etc it must go through all the 
-//widget objects and update the values
-//If there was a server it should only load once
+//Create new widget for each card
+const widgetArray = [];
+document.querySelectorAll('.widget-container').forEach((el) => {
+    widgetArray.push(new TimeWidget(el));
+})
+
+//Add Buttons for Daily Weekly Monthly
+
+//Function that iterates through the widgets and calls updateData() from each object
+widgetArray.map()
 
 
 //Object for storing the JSON data
 const userData = fetchJSONData();
-//Fetch the JSON data
-async function fetchJSONData() {
-    try {
-      const response = await fetch('./data.json'); 
-      if (!response.ok) {
-        throw new Error(`Failed to fetch JSON data, error: ${response.status}`);
-      }
-      const jsonData = await response.json();
-      console.log(jsonData);
-      return jsonData;
-    } catch (error) {
-      console.error('Error fetching JSON data:', error);
-    }
-  }
+
+
+
+// Fetch JSON data from a file
+async function fetchJSONData(){
+
+    return fetch('./data.json')
+        .then(response => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        //You can access a specific entry by title, or return the entire thing
+        .then(data => {
+            
+            // Access a specific entry by title or ID
+            const specificEntry = data.find(entry => entry.title === 'Work');
+            console.log(data);
+            return specificEntry;
+        })
+        .catch(error => {
+            console.error('Error fetching JSON data:', error);
+        });
+    //return (newData);
+}
+
+function extractNumberFromString(string) {
+
+}
