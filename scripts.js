9df0b919a79b6cf449d4374871e98a49e5da9e44
Luke <luke.g.schwade@gmail.com>
2024-05-02 12:14:21 -0400

    json framework

--- scripts.js
+++ scripts.js
@@ -16,3 +16,21 @@ class TimeWidget {
 //When user clicks on monthly, daily etc it must go through all the 
 //widget objects and update the values
 //If there was a server it should only load once
+
+
+//Object for storing the JSON data
+const userData = fetchJSONData();
+//Fetch the JSON data
+async function fetchJSONData() {
+    try {
+      const response = await fetch('./data.json'); 
+      if (!response.ok) {
+        throw new Error(`Failed to fetch JSON data, error: ${response.status}`);
+      }
+      const jsonData = await response.json();
+      console.log(jsonData);
+      return jsonData;
+    } catch (error) {
+      console.error('Error fetching JSON data:', error);
+    }
+  }
